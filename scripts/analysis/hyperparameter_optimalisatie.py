68da6be5c73117273abfef53f1c4be849653810c
ChristiaanWewer <christiaanwewer@hotmail.com>
2026-03-02 15:07:41 +0100

    mlflow added, experiment 1

--- scripts/analysis/hyperparameter_optimalisatie.py
+++ scripts/analysis/hyperparameter_optimalisatie.py
@@ -8,8 +8,13 @@ import warnings
 warnings.filterwarnings("ignore", message="'H' is deprecated and will be removed in a future version")
 import os 
 import optuna
+import mlflow
+import numpy as np
 
-runs_dir = "runs"
+runs_dir = "../runs"
+# mlflow.set_tracking_uri("databricks")  # if running on Databricks this is often already configured
+mlflow.set_experiment("/Shared/hdsr_lstm_optuna")
+base_config = "config_simulatie_1.yml"#, "config_simulatie_2.yml", "config_simulatie_3.yml", "config_simulatie_5.yml"]
 
 def run_neural_hydrology_model(config_name):
     run_config = Config(Path(config_name))
@@ -36,109 +41,111 @@ def extract_tensorboard_scalars(logdir):
     
     return scalars
     
-base_config = "config_simulatie_1.yml"#, "config_simulatie_2.yml", "config_simulatie_3.yml", "config_simulatie_5.yml"]
-# load the yaml file manually using the yaml package
-with open('config_simulatie_1.yml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
-
 
 def objective(trial):
+    with mlflow.start_run(run_name=f"trial_{trial.number}", nested=True):
 
-    # override with optuna some of the config values we want to optimize
-    config['epochs'] = 5 #100
-    config['log_interval'] = 1
-    config['save_weights_every'] = 1
-    config['train_end_date'] = '01/01/2015' # weghalen
+        with open(base_config) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
 
-    config['hidden_size'] = 2 # 128 moet het zijn
+        config = dict(base_config)
 
-    # dropout, also apply to the embedding networks
-    dropout = trial.suggest_categorical('dropout', [0.1, 0.2, 0.3, 0.4])
-    config['output_dropout'] = dropout
-    config['statics_embedding']['dropout'] = dropout
-    config['dynamics_embedding']['dropout'] = dropout
+        # override with optuna some of the config values we want to optimize
+        config['epochs'] = 75 #100
+        config['log_interval'] = 1
+        config['save_weights_every'] = 1
+        # config['train_end_date'] = '01/01/2015' # weghalen
 
-    # statics embedding
-    config['statics_embedding']['hiddens'] = trial.suggest_categorical('statics_embedding_hiddens', [[32], [64], [32, 32], [64, 64]])
-    
-    # dynamics embedding 
-    dynamics_embedding  = trial.suggest_categorical('dynamics_embedding', ['not', [32], [64], [32, 32], [64, 64]])
-    if dynamics_embedding == 'not':
-        config.pop('dynamics_embedding')
-    else:
-        config['dynamics_embedding']['hiddens'] = dynamics_embedding
+        config['hidden_size'] = 64 
 
-    config['learning_rate'] = {0: trial.suggest_categorical('learning_rate', [0.001, 0.0005, 0.0001, 0.00005, 0.00001])}
-    
-    static_variables = ['water_percentage',
-                        'stedelijk_percentage',
-                        'oppervlak',
-                        'water_opp',
-                        'stedelijk_opp',
-                        'zand',
-                        'klei',
-                        'veen',
-                        'stuw']  
+        # dropout, also apply to the embedding networks
+        dropout = trial.suggest_categorical('dropout', [0.1, 0.2, 0.3, 0.4])
+        config['output_dropout'] = dropout
 
-    # kies of je maaiveldhoogte mee wil nemen in de statische variabelen en zo ja, welke keuzes
+        config['learning_rate'] = {0: trial.suggest_categorical('learning_rate', [0.001, 0.0005, 0.0001, 0.00005, 0.00001])}
+        
+        static_variables = ['water_percentage',
+                            'stedelijk_percentage',
+                            'oppervlak',
+                            'water_opp',
+                            'stedelijk_opp',
+                            'zand',
+                            'klei',
+                            'veen',
+                            'stuw']  
 
-    # willen we de average en/of mediaan mee van het maaiveldhoogte
-    static_variables_maaiveldhoogte_mean_median = trial.suggest_categorical('static_variables_maaiveldhoogte_mean_median', [[], ['maaiveldhoogte'],
- ['maaiveldhoogte_median'], ['maaiveldhoogte', 'maaiveldhoogte_median']])
-    
-    # willen we de iqr en/of p95-p05 mee van het maaiveldhoogte
-    static_variables_maaiveldhoogte_iqr_p95_p05 = trial.suggest_categorical('static_variables_maaiveldhoogte_iqr_p95_p05', [[], ['maaiveldhoogte_iqr'],
- ['maaiveldhoogte_p95_minus_p05'], ['maaiveldhoogte_iqr', 'maaiveldhoogte_p95_minus_p05']])
-    
-    # willen we de infiltratie en/of permeabiliteit meeneemen in de statische variabelen
-    static_variables_infiltratie_permeabiliteit = trial.suggest_categorical('static_variables_infiltratie_permeabiliteit', [[], ['infiltratie'], ['permabiliteit'], ['infiltratie', 'permabiliteit']])
+        # kies of je maaiveldhoogte mee wil nemen in de statische variabelen en zo ja, welke keuzes
 
-    # alle statische variabelen bij elkaar mergen
-    static_variables = static_variables + static_variables_maaiveldhoogte_mean_median + static_variables_maaiveldhoogte_iqr_p95_p05 + static_variables_infiltratie_permeabiliteit
-    config['static_attributes'] = static_variables
+        # maaiveldhoogteopties mean/mediaan
+        static_variable_maaiveld_hoogte_mean_median_options = [[], ['maaiveldhoogte'],
+    ['maaiveldhoogte_median'], ['maaiveldhoogte', 'maaiveldhoogte_median']]
+        static_mv_options1 = trial.suggest_categorical('static_variables_maaiveldhoogte_mean_median', [0,1,2,3 ])
+        static_variables_maaiveldhoogte_mean_median = static_variable_maaiveld_hoogte_mean_median_options[static_mv_options1]
 
-    
-    # dynamic variables if we want u and v to be part of the dynamic variables
-    dynamic_variables = ['neerslag',
-                         'straling',
-                         'temperatuur',
-                         'verdamping',
-                         'streefpeil',
-                        ]
-    
-    dynamic_variables_u_v = trial.suggest_categorical('dynamic_variables_u_v', [[], ['u'], ['v'],  ['u', 'v']])
-    dynamic_variables = dynamic_variables + dynamic_variables_u_v
-    config['dynamic_inputs']['1D'] = dynamic_variables
-    config['dynamic_inputs']['1H'] = dynamic_variables
+        # willen we de iqr en/of p95-p05 mee van het maaiveldhoogte
+        static_variables_maaiveldhoogte_iqr_p95_p05_options = [[], ['maaiveldhoogte_iqr'],
+        ['maaiveldhoogte_p95_minus_p05'], ['maaiveldhoogte_iqr', 'maaiveldhoogte_p95_minus_p05']]
+        static_nv_options2 = trial.suggest_categorical('static_variables_maaiveldhoogte_iqr_p95_p05', [0,1,2,3])
+        static_variables_maaiveldhoogte_iqr_p95_p05 = static_variables_maaiveldhoogte_iqr_p95_p05_options[static_nv_options2]
 
-    # maak bestandsnaam voor de nieuwe config
-    config_name = f'config_simulatie_nr_{trial.number}.yml'
-    
-    # schrijf de nieuwe config naar een nieuw bestand
-    with open(config_name, 'w') as file:
-        yaml.dump(config, file)
+        # willen we de infiltratie en/of permeabiliteit meeneemen in de statische variabelen
+        static_variables_infiltratie_permeabiliteit_options = [[], ['infiltratie'], ['permabiliteit'], ['infiltratie', 'permabiliteit']]
+        static_inf_perm_options = trial.suggest_categorical('static_variables_infiltratie_permeabiliteit', [0,1,2, 3])
+        static_variables_infiltratie_permeabiliteit = static_variables_infiltratie_permeabiliteit_options[static_inf_perm_options]
 
-    # draai het model met de nieuwe config
-    run_neural_hydrology_model(config_name)
+        # alle statische variabelen bij elkaar mergen
+        static_variables = \
+                    static_variables + \
+                    static_variables_maaiveldhoogte_mean_median + \
+                    static_variables_maaiveldhoogte_iqr_p95_p05 + \
+                    static_variables_infiltratie_permeabiliteit
 
-    # we assume the latest folder in the runs directory is of past training above
-    folders_in_runs = [os.path.join(runs_dir, folder) for folder in os.listdir(runs_dir)]
+        config['static_attributes'] = static_variables
 
-    # Filter only directories
-    folders_in_runs = [f for f in folders_in_runs if os.path.isdir(f)]
+        # maak bestandsnaam voor de nieuwe config
+        config_name = f'config_simulatie_nr_{trial.number}.yml'
+        
+        # schrijf de nieuwe config naar een nieuw bestand
+        with open(config_name, 'w') as file:
+            yaml.dump(config, file)
 
-    if folders_in_runs:  # Ensure the list is not empty
-        latest_folder = max(folders_in_runs, key=os.path.getctime)
-        print("Latest folder:", latest_folder)
-    else:
-        print("No folders found in 'runs'")
+        # draai het model met de nieuwe config
+        run_neural_hydrology_model(config_name)
 
-    # now we need to log the metric we want to optimize from the neural hydrology model using the latest folder
-    # we optimize the hyperparameters to maximize the mean NSE score
-    data = extract_tensorboard_scalars(latest_folder)
-    validation_NSE_scores = [loss for epoch, loss in data['valid/mean_nse_1d']]
-    max_validation_NSE_score = max(validation_NSE_scores)
-    print('max_validation_NSE_score:', max_validation_NSE_score)
+        # we assume the latest folder in the runs directory is of past training above
+        folders_in_runs = [os.path.join(runs_dir, folder) for folder in os.listdir(runs_dir)]
+
+        # Filter only directories
+        folders_in_runs = [f for f in folders_in_runs if os.path.isdir(f)]
+
+        if folders_in_runs:  # Ensure the list is not empty
+            latest_folder = max(folders_in_runs, key=os.path.getctime)
+            print("Latest folder:", latest_folder)
+        else:
+            print("No folders found in 'runs'")
+
+        # now we need to log the metric we want to optimize from the neural hydrology model using the latest folder
+        # we optimize the hyperparameters to maximize the mean NSE score
+        data = extract_tensorboard_scalars(latest_folder)
+        validation_NSE_scores_1d = np.array([loss for epoch, loss in data['valid/mean_nse_1d']])
+        validation_NSE_scores_1h = np.array([loss for epoch, loss in data['valid/mean_nse_1h']])
+        validation_NSE_scores_mean_1d_1h = (validation_NSE_scores_1d + validation_NSE_scores_1h) / 2
+
+        # we take the maximum validation NSE score
+        max_validation_NSE_score = float(np.max(validation_NSE_scores_mean_1d_1h))
+
+        for (epoch_nse_1d, loss_nse_1d), (epoch_nse_1h, loss_nse_1h) in zip(
+            data["valid/mean_nse_1d"],
+            data["valid/mean_nse_1h"],
+        ):
+            mlflow.log_metric("val_nse_1d", float(loss_nse_1d), step=int(epoch_nse_1d))
+            mlflow.log_metric("val_nse_1h", float(loss_nse_1h), step=int(epoch_nse_1h))
+            mlflow.log_metric("val_nse_1h_1d", (float(loss_nse_1d), float(loss_nse_1h))/2, step=int(epoch_nse_1h))
+
+
+        mlflow.log_params(trial.params)
+        mlflow.log_metric("max_validation_nse_1d_1h", max_validation_NSE_score)
+        mlflow.log_artifact(config_name, artifact_path="config")
 
     return max_validation_NSE_score
 
@@ -150,7 +157,7 @@ study = optuna.create_study(
     load_if_exists=True
 )
 
-study.optimize(objective, n_trials=1)
+study.optimize(objective, n_trials=5)
 
 # Output the best result.
 print("Best trial:")
